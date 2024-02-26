1bfb47127269a738ef4429e3db3a4f5eb8e6209b
Thomas Dall Pedersen <tempo@mail.dk>
2024-02-26 13:15:50 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace OptimizeMePlease
         {
 
             //Comment me after first execution, please.
-            IWillPopulateData();
+            //IWillPopulateData();
 
             BenchmarkRunner.Run<BenchmarkService>();
         }
